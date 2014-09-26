13bc17870c3750b2f79c9bfe5815f1b7cac1eb71
Nathan Snyder <nathan@snydern.com>
2014-09-25 22:51:16 -0400

    Separate JS

---
+++ js/login.js
@@ -0,0 +1,28 @@
+window.onload = function() {
+	var c = document.getElementById("myCanvas");
+	var ctx = c.getContext("2d");
+	ctx.canvas.width  = window.innerWidth+20;
+	ctx.canvas.height = window.innerHeight;
+	var radius = 10;
+	var centerX = 5;
+	var centerY = 5;
+	while(centerY < ctx.canvas.height) {
+		while(centerX < ctx.canvas.width) {
+			ctx.beginPath();
+			var colors = ["#fcc","#fef","#9fd","#cfc","#cde"];
+			
+			ctx.fillStyle = colors[getRandomInt(0,colors.length)];
+			
+			
+			
+			ctx.arc(centerX,centerY,radius,0,2*Math.PI);
+			//ctx.fill();
+			centerX+=2*radius+2;
+		}
+		centerY+=2*radius+2;
+		centerX=5;
+	}
+}
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
