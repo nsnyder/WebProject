f3f5d9e9c824a0735ea2aa6038c7e039b0e0694e
tamsd <stephend.tam@gmail.com>
2014-10-19 16:35:32 -0400

    added skeleton stuff to cavnas.js

--- js/canvas.js
+++ js/canvas.js
@@ -1,11 +1,24 @@
 //mainCanvas container
 var canvas;
+//Tells when user is holding down mouse
+var mouseDown = false;
 
 window.onload=function(){
 	canvas = document.getElementById("mainCanvas");
-	canvas.addEventListener("mouseover", test);
+	canvas.addEventListener("mouseover", changeCursor);
 }
 
-function test(){
-	alert();
+function mouseClick(){
+}
+
+function mouseRelease(){
+}
+
+function mouseOut(){
+}
+
+function 
+
+function changeCursor(){
+	canvas.style.cursor="pointer";
 }
