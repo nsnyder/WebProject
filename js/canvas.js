889b38b1ebba950a6f26db363fa05ed7d421e663
tamsd <stephend.tam@gmail.com>
2014-10-19 15:59:31 -0400

    Added event listener on mouse over to cavnas

---
+++ js/canvas.js
@@ -0,0 +1,11 @@
+//mainCanvas container
+var canvas;
+
+window.onload=function(){
+	canvas = document.getElementById("mainCanvas");
+	canvas.addEventListener("mouseover", test);
+}
+
+function test(){
+	alert();
+}
