6c7612e1426a701dfcc89c02c94505784c8e40f1
tamsd <stephend.tam@gmail.com>
2014-10-19 23:57:58 -0400

    setting origin X and Y on canvas click working

--- js/Line.js
+++ js/Line.js
@@ -1,3 +1,10 @@
+function measureToNumber(str){
+	var parts = str.split("p");
+	var val = parts[0];
+	val = Number(val);
+	return val;
+}
+
 function Line(){
 	this.color="#000000";
 	this.draw=function(cnv){
@@ -7,18 +14,17 @@ function Line(){
 	this.endX;
 	this.endY;
 	this.mouseClick = function(e) {
-		
 		//Get Main content left padding
-		var mainContent = document.getElementById("MainContent");
-		var leftPad = mainContent.paddingLeft;
+		var mainContentStyle = getComputedStyle(document.getElementById("MainContent"), null);
+		var leftPad = measureToNumber(mainContentStyle.paddingLeft);
 		//Get Frame width for left and top and get frame top margin
-		var frame = document.getElementById("Frame");
-		var frameLeftWidth = frame.borderLeftWidth;
-		var frameTopWidth = frame.borderTopWidth;
-		var frameTopMargin = frame.marginTop;
+		var frameStyle = getComputedStyle(document.getElementById("Frame"), null);
+		var frameLeftWidth = measureToNumber(frameStyle.borderLeftWidth);
+		var frameTopWidth = measureToNumber(frameStyle.borderTopWidth);
+		var frameTopMargin = measureToNumber(frameStyle.marginTop);
 		//Get MainNav box height (assuming nav bar remains same format)
-		var mainNav = document.getElementById("MainNav");
-		var mainNavHeight = mainNav.height+mainNav.paddingTop;
+		var mainNavStyle = getComputedStyle(document.getElementById("MainNav"), null);
+		var mainNavHeight = measureToNumber(mainNavStyle.height)+ measureToNumber(mainNavStyle.paddingBottom) + measureToNumber(mainNavStyle.paddingTop);
 		
 		//Adjust originX and originY so that it maps in accordance with canvas context
 		this.originX = e.clientX - leftPad - frameLeftWidth;
