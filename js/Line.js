41de44ba7d94041a6ea154b304b385ed2777c51f
tamsd <stephend.tam@gmail.com>
2014-10-20 11:34:39 -0400

    created 2 functions that returns x offset and y offset

--- js/Line.js
+++ js/Line.js
@@ -4,6 +4,27 @@ function measureToNumber(str){
 	val = Number(val);
 	return val;
 }
+//determines cursor x offset relative to canvas (if canvas is moved or other objects are edited then this function may not be accurate anymore)
+function getCursorXoffset(){
+	//Get Main content left padding
+	var mainContentStyle = getComputedStyle(document.getElementById("MainContent"), null);
+	var leftPad = measureToNumber(mainContentStyle.paddingLeft);
+	//Get Frame width for left and top and get frame top margin
+	var frameStyle = getComputedStyle(document.getElementById("Frame"), null);
+	var frameLeftWidth = measureToNumber(frameStyle.borderLeftWidth);
+	return leftPad+frameLeftWidth;
+}
+//determines cursor y offset relative to canvas(if canvas is moved or other objects are edited then this function may not be accurate anymore)
+function getCursorYoffset(){
+	//Get Frame width for left and top and get frame top margin
+	var frameStyle = getComputedStyle(document.getElementById("Frame"), null);
+	var frameTopWidth = measureToNumber(frameStyle.borderTopWidth);
+	var frameTopMargin = measureToNumber(frameStyle.marginTop);
+	//Get MainNav box height
+	var mainNavStyle = getComputedStyle(document.getElementById("MainNav"), null);
+	var mainNavHeight = measureToNumber(mainNavStyle.height)+ measureToNumber(mainNavStyle.paddingBottom) + measureToNumber(mainNavStyle.paddingTop);
+	return frameTopWidth+frameTopMargin+mainNavHeight;
+}
 
 function Line(){
 	this.color="#000000";
@@ -13,22 +34,10 @@ function Line(){
 	this.originY;
 	this.endX;
 	this.endY;
-	this.mouseClick = function(e) {
-		//Get Main content left padding
-		var mainContentStyle = getComputedStyle(document.getElementById("MainContent"), null);
-		var leftPad = measureToNumber(mainContentStyle.paddingLeft);
-		//Get Frame width for left and top and get frame top margin
-		var frameStyle = getComputedStyle(document.getElementById("Frame"), null);
-		var frameLeftWidth = measureToNumber(frameStyle.borderLeftWidth);
-		var frameTopWidth = measureToNumber(frameStyle.borderTopWidth);
-		var frameTopMargin = measureToNumber(frameStyle.marginTop);
-		//Get MainNav box height (assuming nav bar remains same format)
-		var mainNavStyle = getComputedStyle(document.getElementById("MainNav"), null);
-		var mainNavHeight = measureToNumber(mainNavStyle.height)+ measureToNumber(mainNavStyle.paddingBottom) + measureToNumber(mainNavStyle.paddingTop);
-		
+	this.mouseClick = function(e) {		
 		//Adjust originX and originY so that it maps in accordance with canvas context
-		this.originX = e.clientX - leftPad - frameLeftWidth;
-		this.originY = e.clientY - frameTopWidth - frameTopMargin - mainNavHeight;
+		this.originX = e.clientX - getCursorXoffset();
+		this.originY = e.clientY - getCursorYoffset();
 		alert("X: " + this.originX + " Y: " + this.originY);
 		mouseDown = true;
 	}
