3d0bd6044e0956ed935b27561355e783ee793be7
adamomj1 <adamomj1@gcc.edu>
2014-10-20 22:59:10 -0400

    stuff

--- js/signup.js
+++ js/signup.js
@@ -5,9 +5,39 @@ window.onload = function() {
 pass = document.getElementById("pwd");
 confpass = document.getElementById("confpwd");
 message = document.getElementById("pwdconfmessage");
-
 pass.addEventListener("keyup",checkpass);
 confpass.addEventListener("keyup",checkpass);
+
+var c = document.getElementById("myCanvas");
+	var ctx = c.getContext("2d");
+	ctx.canvas.width  = window.innerWidth+20;
+	ctx.canvas.height = window.innerHeight;
+	var radius = 10;
+	var centerX = 5;
+	var centerY = 5;
+	while(centerY < ctx.canvas.height) {
+		while(centerX < ctx.canvas.width) {
+			ctx.beginPath();
+			var colors = ["#fcc","#fef","#9fd","#cfc","#cde"];
+			
+			ctx.fillStyle = colors[getRandomInt(0,colors.length)];
+			
+			
+			
+			ctx.arc(centerX,centerY,radius,0,2*Math.PI);
+			ctx.fill();
+			centerX+=2*radius+2;
+		}
+		centerY+=2*radius+2;
+		centerX=5;
+	}
+	/*Login event listener*/
+	var loginBtn = document.getElementById();
+	/*end login listener*/
+}
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
 function checkpass()
