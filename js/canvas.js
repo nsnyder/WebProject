32627d3e39bf00927c1066af97204dd9ab2c9c1c
tamsd <stephend.tam@gmail.com>
2014-10-20 21:54:05 -0400

    changed apren. html and css color ids; working on setColor...

--- js/canvas.js
+++ js/canvas.js
@@ -13,6 +13,11 @@ window.onload=function(){
 	for(var i=0;i<tools.length;++i) {
 		tools[i].addEventListener("click",setTool);
 	}
+	// Add event listeners to change color
+	var colors = document.getElementsByClassName("colors");
+	for(var i=0;i<colors.length;++i) {
+		colors[i].addEventListener("click",setColor);
+	}
 }
 
 function setTool() {
@@ -28,7 +33,7 @@ function setTool() {
 	} catch(e) {}
 	for(var i=0;i<classes.length;++i) {
 		if(classes[i]=="brush") {
-			// tool = new Brush();
+			tool = new Brush();
 		}
 		if(classes[i]=="line") {
 			tool = new Line(canvas);
@@ -46,6 +51,29 @@ function setTool() {
 	try {
 		canvas.addEventListener("mousedown", tool.mouseDown);
 		canvas.addEventListener("mouseup", tool.mouseRelease);
+	} catch(e) {}
+}
+
+function setColor() {
+	var color = this.id;
+	
+	var classes = this.classList;
+	for(var i=0;i<classes.length;++i) {
+		if(classes[i]=="brush") {
+			// tool = new Brush();
+		}
+		if(classes[i]=="line") {
+			tool = new Line(canvas);
+		}
+		if(classes[i]=="rectangle") {
+			tool = new Rectangle(canvas);
+		}
+		if(classes[i]=="ellipse") {
+			tool = new Circle(canvas);
+		}
+	}
+	try {
+		tool.color = "#000000";
 	} catch(e) {}
 }
 
