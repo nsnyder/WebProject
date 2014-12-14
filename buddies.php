268d59f81ef16e0c0e97f78a48165d3325dfe6d1
adamomj1 <adamomj1@gcc.edu>
2014-12-14 16:29:25 -0500

    Buddy Requests added to Buddy Page

--- buddies.php
+++ buddies.php
@@ -7,7 +7,7 @@
 <html>
 <head lang="en">
    <meta charset="utf-8">
-   <title>Change this</title>
+   <title>Buddies Page</title>
    <link rel="stylesheet" type="text/css" href="css/global.css">
    <link rel="stylesheet" type="text/css" href="css/buddies.css">
 </head>
@@ -20,7 +20,22 @@
 </nav>
 <div id="MainContent">
 <section>
-<h1>Your Buddies</h1>
+<h1>Your Buddy Requests</h1>
+<div class="buddyTile">
+	<img src="images/circle.png" title="Profile Picture" alt="Request 1's Picture" />
+	<a href="#">Buddy Request 1</a>
+</div>
+<div class="buddyTile">
+	<img src="images/circle.png" title="Profile Picture" alt="Request 2's Picture" />
+	<a href="#">Buddy Request 2</a>
+</div>
+<div class="buddyTile">
+	<img src="images/circle.png" title="Profile Picture" alt="Request 3's Picture" />
+	<a href="#">Buddy Request 3</a>
+</div>
+</section>
+<section>
+<h2>Your Buddies</h2>
 <input type="text" id="buddyFilter" placeholder="Filter Buddies"/>
 <div class="buddyTile">
 	<img src="images/james_mckay.jpg" title="Profile Picture" alt="James McKay's Picture" />
@@ -31,35 +46,35 @@
 	<a href="#">Professor Boatright</a>
 </div>
 <div class="buddyTile">
-	<img src="images/heart.png" title="Profile Picture" alt="Buddy 1's Picture" />
+	<img src="images/heart.png" title="Profile Picture" alt="Buddy 3's Picture" />
 	<a href="#">&lt;&lt; These guys</a>
 </div>
 <div class="buddyTile">
-	<img src="images/circle.png" title="Profile Picture" alt="Buddy 1's Picture" />
+	<img src="images/circle.png" title="Profile Picture" alt="Buddy 4's Picture" />
 	<a href="#">Buddy 4</a>
 </div>
 <div class="buddyTile">
-	<img src="images/circle.png" title="Profile Picture" alt="Buddy 1's Picture" />
+	<img src="images/circle.png" title="Profile Picture" alt="Buddy 5's Picture" />
 	<a href="#">Buddy 5</a>
 </div>
 <div class="buddyTile">
-	<img src="images/circle.png" title="Profile Picture" alt="Buddy 1's Picture" />
+	<img src="images/circle.png" title="Profile Picture" alt="Buddy 6's Picture" />
 	<a href="#">Buddy 6</a>
 </div>
 <div class="buddyTile">
-	<img src="images/circle.png" title="Profile Picture" alt="Buddy 1's Picture" />
+	<img src="images/circle.png" title="Profile Picture" alt="Buddy 7's Picture" />
 	<a href="#">Buddy 7</a>
 </div>
 <div class="buddyTile">
-	<img src="images/circle.png" title="Profile Picture" alt="Buddy 1's Picture" />
+	<img src="images/circle.png" title="Profile Picture" alt="Buddy 8's Picture" />
 	<a href="#">Buddy 8</a>
 </div>
 <div class="buddyTile">
-	<img src="images/circle.png" title="Profile Picture" alt="Buddy 1's Picture" />
+	<img src="images/circle.png" title="Profile Picture" alt="Buddy 9's Picture" />
 	<a href="#">Buddy 9</a>
 </div>
 <div class="buddyTile">
-	<img src="images/circle.png" title="Profile Picture" alt="Buddy 1's Picture" />
+	<img src="images/circle.png" title="Profile Picture" alt="Buddy 10's Picture" />
 	<a href="#">Buddy 10</a>
 </div>
 </section>
